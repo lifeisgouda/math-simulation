765ec1547076a0c66da70678bffac63a1de415c1
Jane Jeon <dahaejeon@gmail.com>
2022-04-06 22:23:32 +0900

    birthday problem

---
+++ birthday_problem.py
@@ -0,0 +1,19 @@
+import random
+
+TRIALS = 100000    # Number of trials
+same_birthday = 0  # Number of trials with same birthday
+
+# 100,000 trials
+for _ in range(TRIALS):
+    birthdays = []
+
+    # If 23 people have the same birthday, increase same_birthday by 1.
+    for _ in range(23):
+        birthday = random.randint(1, 365)
+        if birthday in birthdays:
+            same_birthday += 1
+            break
+        birthdays.append(birthday)
+
+    # Probability of experiments with the same birthday among all 100,000 trial
+    print(f'{same_birthday / TRIALS * 100}%')
